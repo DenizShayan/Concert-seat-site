34e4b9e6aa459e8d236438e63f3d7535c50e57fe
Deniz Shayan <92824050+DenizShayan@users.noreply.github.com>
2025-12-12 00:31:41 -0500

    Update cart.tsx

--- concert-site/pages/cart.tsx
+++ concert-site/pages/cart.tsx
@@ -2,4 +2,13 @@ import { uesCart } from "../context/CartContext"
 
 export default function CartPage() {
     const {items. removeSeat, cleanCart, totalPrice} = uesCart();
-return()
+return(
+    <main className={ min-h-screen bg-slate-900 }
+
+
+
+
+
+
+
+)
