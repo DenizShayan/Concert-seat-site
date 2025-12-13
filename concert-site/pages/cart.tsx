c8a04125bb8e043b13d8bc41cf5db8f9c886e97a
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-12-13 18:32:04 -0500

    Update cart.tsx

--- concert-site/pages/cart.tsx
+++ concert-site/pages/cart.tsx
@@ -3,7 +3,14 @@ import { uesCart } from "../context/CartContext"
 export default function CartPage() {
     const {items. removeSeat, cleanCart, totalPrice} = uesCart();
 return(
-    <main className={ min-h-screen bg-slate-900 }
+    <main className="min-h-screen bg-slate-900 text-white px-4 py-8">
+        <h1 className="text-3xl mb-6">Your Cart</h1>
+
+
+
+
+
+    </main>
 
 
 
